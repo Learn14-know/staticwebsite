0f381088ea9ed91d1960c2d92caf66732ab58f0e
Learn14-know <shriyaarora97@gmail.com>
2025-09-07 00:12:34 -0400

    INITIAL COMMIT
    INITIAL COMMIT

---
+++ deploy-website.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update package list and install Apache and Git
+sudo apt-get update
+sudo apt-get install -y apache2 git
+
+# Remove default Apache page
+sudo rm -rf /var/www/html/*
+
+# Clone your GitHub repository into Apache web directory
+sudo git clone https://github.com/Learn14-know/ProjectT.git /var/www/html
+
+# Set proper permissions so Apache can serve the files
+sudo chown -R www-data:www-data /var/www/html
+sudo chmod -R 755 /var/www/html
+
+# Restart Apache to apply changes
+sudo systemctl restart apache2
